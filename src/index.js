277fb3c83d9e649b8bfd8f654c39365e7e995251
Boots0309 <joshua3978arellano@gmail.com>
2021-10-03 19:49:30 +0300

    initialize required variables

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// create 2 variables, firstCard and secondCard.
+// Set their values to a random number between 2 - 11
+
+let firstCard = Math.floor((Math.random() * 10) + 2);
+let secondCard = Math.floor((Math.random() * 10) + 2);
+let sum = firstCard + secondCard;
+console.log(sum);
