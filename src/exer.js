d1ca540500716d663107120dd4a5d76f37502069
Boots0309 <joshua3978arellano@gmail.com>
2021-10-09 20:14:20 +0300

    added exerScript

---
+++ src/exer.js
@@ -0,0 +1,98 @@
+        // check if a person is old enough to enter the night club (21)
+// log a suitable message in the console for both cases
+
+// if less than 21 log 
+let message1 = "You are to young to enter the club!";
+// else log
+let message2 = "Welcome!";
+let age = 89;
+
+if (age >= 21) {
+    console.log(message2);
+} else {
+    console.log(message1);
+};
+
+// Check if the person is elegible for a birthday card from the King!
+// if less the 100 log "Not elegible"
+// if exactly 100 log "Here is your birthday card from the king!"
+// else log "Not elegible, you've already gotten one"
+
+let ageCheck = 101;
+
+    if (ageCheck < 100) {
+        console.log("Not elegible");
+    } else if (ageCheck === 100) {
+        console.log("Here is your birthday card from the king!");
+    } else {
+        console.log("Not elegible, you've already gotten one");
+    };
+
+
+    function assignVal(val) {
+        let cardVal = "";
+        switch(val) {
+            case "2d":
+            case "2h":
+            case "2c":
+            case "2s":
+                cardVal = 2;
+                break;
+            case "3d":
+            case "3h":
+            case "3c":
+            case "3s":
+                cardVal = 3;
+                break;
+            case "4d":
+            case "4h":
+            case "4c":
+            case "4s":
+                cardVal = 4;
+                break;
+            case "5d":
+            case "5h":
+            case "5c":
+            case "5s":
+                cardVal = 5;
+                break;
+            case "6d":
+            case "6h":
+            case "6c":
+            case "6s":
+                cardVal = 6;
+                break;
+            case "7d":
+            case "7h":
+            case "7c":
+            case "7s":
+                cardVal = 7;
+                break;
+            case "8d":
+            case "8h":
+            case "8c":
+            case "8s":
+                cardVal = 8;
+                break;
+            case "9d":
+            case "9h":
+            case "9c":
+            case "9s":
+                cardVal = 9;
+                break;
+            case "10d":
+            case "10h":
+            case "10c":
+            case "10s":
+                cardVal = 10;
+                break;
+                                                                  
+        };
+    
+        return cardVal;
+    };
+    
+    console.log(assignVal("7c"))
+
+
+    // Arrays
--- src/index.js
+++ src/index.js
@@ -3,45 +3,37 @@
 
 let firstCard = Math.floor((Math.random() * 10) + 2);
 let secondCard = Math.floor((Math.random() * 10) + 2);
+let cards = [firstCard, secondCard]
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 let sum = firstCard + secondCard;
 
-console.log(sum);
-
-if (sum < 21) {
-    console.log("Would you like to draw a card?");
-} else if (sum === 21) {
-    console.log("You've got BlackJack!");
-} else {
-    console.log("You're out of the game!");
+function startGame() {
+    cardDeal();
 }
 
+function cardDeal() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
 
-// check if a person is old enough to enter the night club (21)
-// log a suitable message in the console for both cases
-
-// if less than 21 log 
-let message1 = "You are to young to enter the club!";
-// else log
-let message2 = "Welcome!";
-let age = 89;
-
-if (age >= 21) {
-    console.log(message2);
-} else {
-    console.log(message1);
+    if (sum <= 20) {
+        message = "Would you like to draw a card?";
+    } else if (sum === 21) {
+        message = "You've got BlackJack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
 };
 
-// Check if the person is elegible for a birthday card from the King!
-// if less the 100 log "Not elegible"
-// if exactly 100 log "Here is your birthday card from the king!"
-// else log "Not elegible, you've already gotten one"
-
-let ageCheck = 101;
-
-    if (ageCheck < 100) {
-        console.log("Not elegible");
-    } else if (ageCheck === 100) {
-        console.log("Here is your birthday card from the king!");
-    } else {
-        console.log("Not elegible, you've already gotten one");
-    };
+function cardDraw() {
+    let thirdCard = Math.floor((Math.random() * 10) + 2);
+    sum += thirdCard;
+    startGame();
+}
